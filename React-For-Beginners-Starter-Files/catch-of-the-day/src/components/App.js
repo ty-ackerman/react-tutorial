94cbab83d95c0fb7fd33cdf3f5cdb29f58607641
ty-ackerman <me@tylerackerman.io>
2018-09-24 15:43:59 -0400

    starting lecture on loading data into state on click

---
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    //stop form from submitting
+    e.preventDefault();
+    //get text from input
+    const storeName = this.myInput.value.value;
+    //change page to store/whaever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  // handleChange = () => {
+  //   console.log(this.myInput.value.value);
+  // };
+
+  render() {
+    return (
+      <form action="" className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          ref={this.myInput}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          // onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick} type="submit">
+          Visit Store
+        </button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
